8a1374c41d5c24c3efca4c010cf11d3eede178ac
Mahesh Bishnoi <mahesh.bishnoi.dev@gmail.com>
2026-08-23 10:13:47 +0530

    Time: 28 ms (15.79%) | Memory: 47.6 MB (59.68%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,64 +1,72 @@
 class Solution {
-    class pair{
+    class Pair{
         int x;
         int y;
-        pair(int x,int y){
+        int d;
+        Pair(int x,int y,int d){
             this.x = x;
             this.y = y;
-         }
+            this.d = d;
+        }
     }
-int[][] dir = {
-    {-1,  0}, // Up
-    { 1,  0}, // Down
-    { 0, -1}, // Left
-    { 0,  1}, // Right
-    {-1, -1}, // Top-Left
-    {-1,  1}, // Top-Right
-    { 1, -1}, // Bottom-Left
-    { 1,  1}  // Bottom-Right
-};
 
-boolean isSafe(int x,int y,int m,int n){
-    return x >= 0 && y >= 0 && x < m && y < n;
-}
+    int dir[][] = {
+        {0,1},
+        {1,0},
+        {0,-1},
+        {-1,0},
+        {-1,-1},
+        {-1,1},
+        {1,1},
+        {1,-1}
+    };
+
+    boolean isSafe(int x,int y,int m,int n){
+        return x >= 0 && y >= 0 && x < m && y < n;
+    }
     public int shortestPathBinaryMatrix(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
 
-        if(m == 0 || n == 0 || grid[0][0] != 0){
+        if(grid[0][0] != 0 || grid[m-1][n-1] != 0){
             return -1;
         }
 
-        Queue<pair> q = new LinkedList<>();
+        //dis array for ans count zeros
+        int[][] dis = new int[m][n];
 
-        q.add(new pair(0,0));
+        for(int i = 0; i < m; i++){
+            Arrays.fill(dis[i],Integer.MAX_VALUE);
+        }
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> a.d - b.d);
+        pq.add(new Pair(0,0,1));
         grid[0][0] = 1;
-        int count = 1;
 
-        while(!q.isEmpty()){
-            int size = q.size();
-            while(size-- > 0){
-                pair curr = q.peek();
-                q.remove();
-                int x = curr.x;
-                int y = curr.y;
-            
-                if(x == m-1 && y == n-1){
-                    return count;
-                }
+        while(!pq.isEmpty()){
+            Pair curr = pq.remove();
+            int x = curr.x;
+            int y = curr.y;
+            int d = curr.d;
 
-                for(int[] d : dir){
-                    int newx = x + d[0];
-                    int newy = y + d[1];
-
-                    if(isSafe(newx,newy,m,n) && grid[newx][newy] == 0){
-                    q.add(new pair(newx,newy));
-                    grid[newx][newy] = 1;
-                }
-                }     
-
+            //destination
+            if(x == m-1 && y == n-1){
+                return d;
             }
-            count++;
+
+            for(int[] dr : dir){
+                int newx = x + dr[0];
+                int newy = y + dr[1];
+
+                if(isSafe(newx,newy,m,n) && grid[newx][newy] == 0){
+                    int newd = d + 1;
+                    if(newd < dis[newx][newy]){
+                        dis[newx][newy] = newd;
+                        pq.add(new Pair(newx,newy,newd));
+                        grid[newx][newy] = 1;
+                    }
+                }
+            }
         }
         return -1;
     }
